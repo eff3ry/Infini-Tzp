0fdfb62027372ab41e40bd9aa4fcc998ba2a496c
IDuzTheGamez <jeffery.tolmie@gmail.com>
2023-12-24 18:18:56 +1300

    setup ig

---
+++ LethalCompanyTemplate/PluginInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LethalCompanyTemplate
+{
+    public class PluginInfo
+    {
+        public const String PLUGIN_GUID = "com.effery-PlaceholderName";
+        public const String PLUGIN_NAME = "PlaceholderName";
+        public const String PLUGIN_VERSION = "1.0.0";
+
+    }
+}
